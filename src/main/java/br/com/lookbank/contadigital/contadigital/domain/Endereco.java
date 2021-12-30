33b73fb65fbd8fecf1a8679f26371be9d93526f5
jordihofc <jordi.silva@ufv.br>
2021-12-30 16:43:30 -0300

    add constraint de nullable

--- src/main/java/br/com/lookbank/contadigital/contadigital/domain/Endereco.java
+++ src/main/java/br/com/lookbank/contadigital/contadigital/domain/Endereco.java
@@ -10,11 +10,17 @@ public class Endereco {
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String rua;
+    @Column(nullable = false)
     private String bairro;
+    @Column(nullable = false)
     private String complemento;
+    @Column(nullable = false)
     private String cidade;
+    @Column(nullable = false)
     private String estado;
+    @Column(nullable = false)
     private String cep;
 
     @ManyToOne(optional = false)
